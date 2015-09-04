aa6c0801c40a768424dd35813820476551afaa57
Ivo Flipse <ivoflipse@gmail.com>
2015-09-04 12:02:06 +0200

    Created packages invoke, boltons and pymssql (though the latter doesn't seem to work)

--- invoke/bld.bat
+++ invoke/bld.bat
@@ -1,3 +1,10 @@
+:: conda-build tries to compile .pyc files in Python 2 environment, but Invoke
+:: has yaml for Python3 codebase, which fails on Python2.
+:: Related question on conda-build issue tracker:
+:: https://github.com/conda/conda-build/pull/317#commitcomment-12926020
+:: WARNING: this has to be removed when noarch_python is enabled!
+if "%PY_VER%" == "2.7" rmdir /S /Q "invoke\vendor\yaml3"
+
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
 
