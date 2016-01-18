db7799c1dd99f2da63711b20c2fce18fb7fcecca
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2016-01-18 13:36:06 +0100

    Fixed pyinvoke package

--- invoke/bld.bat
+++ invoke/bld.bat
@@ -1,6 +1,13 @@
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
 
+IF %PY3K% EQ 0 (
+    @RD /S /Q "C:\Users\Korijn\Miniconda\envs\_build\Lib\site-packages\invoke-0.11.1-py2.7.egg\invoke\vendor\yaml3"
+)
+IF %PY3K% EQ 1 (
+    @RD /S /Q "C:\Users\Korijn\Miniconda\envs\_build\Lib\site-packages\invoke-0.11.1-py2.7.egg\invoke\vendor\yaml2"
+)
+
 :: Add more build steps here, if they are necessary.
 
 :: See
