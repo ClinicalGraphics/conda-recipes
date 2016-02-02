9393f482c1afcdc9b7eed57ab36990658b2ab339
Maxime <maxyme@hotmail.com>
2016-02-02 11:11:30 +0100

    Fixed VTK script for python 2.7 or 3.5 with or without conda -m suffix to python 3.5

--- vtk/build.sh
+++ vtk/build.sh
@@ -3,6 +3,16 @@
 mkdir build
 cd build
 
+include_path=${PREFIX}/include/python${PY_VER}
+if [ ! -f ${PREFIX}/include/python${PY_VER} ]; then
+    include_path=${PREFIX}/include/python${PY_VER}m
+fi
+
+library_path=${PREFIX}/lib/${PY_LIB}
+if [ ! -f ${PREFIX}/include/python${PY_VER} ]; then
+    library_path=${PREFIX}/lib/libpython3.5m.so
+fi
+
 if [ `uname` == Linux ]; then
     CC=gcc
     CXX=g++
@@ -21,42 +31,12 @@ if [ `uname` == Linux ]; then
         -DBUILD_SHARED_LIBS=ON \
         -DVTK_WRAP_PYTHON=ON \
         -DPYTHON_EXECUTABLE=${PYTHON} \
-        -DPYTHON_INCLUDE_PATH=${PREFIX}/include/python${PY_VER} \
-        -DPYTHON_LIBRARY=${PREFIX}/lib/${PY_LIB} \
+        -DPYTHON_INCLUDE_PATH=$include_path \
+        -DPYTHON_LIBRARY=$library_path \
         -DVTK_INSTALL_PYTHON_MODULE_DIR=${SP_DIR} \
-        -DModule_vtkRenderingMatplotlib=ON \
-        -DVTK_USE_X=ON
-fi
+        -DVTK_USE_X=ON \
+        -DModule_vtkRenderingMatplotlib=ON 
 
-if [ `uname` == Darwin ]; then
-    CC=cc
-    CXX=c++
-    PY_LIB="libpython${PY_VER}.dylib"
-    SDK_PATH="/Developer/SDKs/MacOSX10.6.sdk"
-
-    cmake .. \
-        -DCMAKE_C_COMPILER=$CC \
-        -DCMAKE_CXX_COMPILER=$CXX \
-        -DVTK_REQUIRED_OBJCXX_FLAGS='' \
-        -DVTK_USE_CARBON=OFF \
-        -DVTK_USE_TK=OFF \
-        -DIOKit:FILEPATH=${SDK_PATH}/System/Library/Frameworks/IOKit.framework \
-        -DVTK_USE_COCOA=ON \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-        -DCMAKE_INSTALL_RPATH:STRING="$PREFIX/lib" \
-        -DBUILD_DOCUMENTATION=OFF \
-        -DVTK_HAS_FEENABLEEXCEPT=OFF \
-        -DBUILD_TESTING=OFF \
-        -DBUILD_EXAMPLES=OFF \
-        -DBUILD_SHARED_LIBS=ON \
-        -DVTK_WRAP_PYTHON=ON \
-        -DPYTHON_EXECUTABLE=${PYTHON} \
-        -DPYTHON_INCLUDE_PATH=${PREFIX}/include/python${PY_VER} \
-        -DPYTHON_LIBRARY=${PREFIX}/lib/${PY_LIB} \
-        -DVTK_INSTALL_PYTHON_MODULE_DIR=${SP_DIR} \
-        -DModule_vtkRenderingMatplotlib=ON \
-        -DVTK_USE_X=OFF
 fi
 
 make -j${CPU_COUNT}
