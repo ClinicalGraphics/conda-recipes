c2010aadbe1761e38a5e67a3cc5038067cda6e8a
ivoflipse <ivoflipse@gmail.com>
2016-03-01 11:46:10 +0100

    Updated several packages, chief among which Miktex

--- miktex-portable/bld.bat
+++ miktex-portable/bld.bat
@@ -1,7 +1,10 @@
 mkdir "%PREFIX%\miktex-portable\"
 if errorlevel 1 exit 1
 
-"%SCRIPTS%\7za" x .\miktex-portable-2.9.5719.exe -yo"%PREFIX%\miktex-portable\"
+mkdir "%SCRIPTS%"
+if errorlevel 1 exit 1
+
+"%LIBRARY_BIN%\7za" x .\miktex-portable-2.9.5857.exe -yo"%PREFIX%\miktex-portable\"
 if errorlevel 1 exit 1
 
 for %%f in ("%PREFIX%\miktex-portable\miktex\bin\*.exe") do echo %%f %%* >> "%SCRIPTS%\%%~nf.bat"
