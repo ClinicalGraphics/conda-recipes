e216386b57dae1dd299e8c2c13158a5f05269f1d
Maxime <maxyme@outlook.com>
2016-03-08 17:00:17 +0100

    VTK and GDMC now using anaconda compilers

--- vtk/build.sh
+++ vtk/build.sh
@@ -1,24 +1,26 @@
+
 #!/bin/bash
 
 mkdir build
 cd build
 
-include_path=${PREFIX}/include/python${PY_VER}
-if [ ! -d $include_path ]; then
-  # Control will enter here if $DIRECTORY doesn't exist.
-  include_path=${PREFIX}/include/python${PY_VER}m
-fi
-
-PY_LIB="libpython${PY_VER}.so"
-library_file_path=${PREFIX}/lib/${PY_LIB}
-if [ ! -f $library_file_path ]; then
-    library_file_path=${PREFIX}/lib/libpython3.5m.so
-fi
-
 if [ `uname` == Linux ]; then
-    CC=gcc
-    CXX=g++
+    CC=${PREFIX}/bin/gcc
+    CXX=${PREFIX}/bin/g++
+
+    # FIXME refactor to reuse the python name (e.g. python3.5m)
+    # FIXME detect any kind of suffix (m, or d)
+    include_path=${PREFIX}/include/python${PY_VER}
+    if [ ! -d $include_path ]; then
+      # Control will enter here if $DIRECTORY doesn't exist.
+      include_path=${PREFIX}/include/python${PY_VER}m
+    fi
+
     PY_LIB="libpython${PY_VER}.so"
+    library_file_path=${PREFIX}/lib/${PY_LIB}
+    if [ ! -f $library_file_path ]; then
+        library_file_path=${PREFIX}/lib/libpython${PY_VER}m.so
+    fi
 
     cmake .. \
         -DCMAKE_C_COMPILER=$CC \
@@ -33,12 +35,11 @@ if [ `uname` == Linux ]; then
         -DBUILD_SHARED_LIBS=ON \
         -DVTK_WRAP_PYTHON=ON \
         -DPYTHON_EXECUTABLE=${PYTHON} \
-        -DPYTHON_INCLUDE_PATH=$include_path \
-        -DPYTHON_LIBRARY=$library_file_path \
+        -DPYTHON_INCLUDE_PATH:PATH=$include_path \
+        -DPYTHON_LIBRARY:FILEPATH=$library_file_path \
         -DVTK_INSTALL_PYTHON_MODULE_DIR=${SP_DIR} \
-        -DVTK_USE_X=ON \
-        -DModule_vtkRenderingMatplotlib=ON 
-
+        -DModule_vtkRenderingMatplotlib=ON \
+        -DVTK_USE_X=OFF
 fi
 
 make -j${CPU_COUNT}
