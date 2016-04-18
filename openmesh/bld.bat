feafc7a7ccf8a241370d56b0fff38ae29b513f77
Eelco Hoogendoorn <hoogendoorn.eelco@gmail.com>
2016-04-18 20:09:24 +0200

    moved build folder

--- openmesh/bld.bat
+++ openmesh/bld.bat
@@ -28,6 +28,11 @@ if %ARCH%==64 (
 REM tell cmake where Python is
 set PYTHON_LIBRARY=%PREFIX%\libs\python%PY_VER:~0,1%%PY_VER:~2,1%.lib
 
+REM move folder
+mkdir build
+cd build
+
+
 REM generate visual studio solution
 cmake %SRC_DIR% -G"%GENERATOR_NAME%" ^
     -Wno-dev ^
@@ -36,9 +41,6 @@ cmake %SRC_DIR% -G"%GENERATOR_NAME%" ^
 
 if errorlevel 1 exit 1
 
-REM move folder
-mkdir build
-cd build
 
 REM move folder
 cmake --build . --clean-first --target ALL_BUILD --config %BUILD_CONFIG%
