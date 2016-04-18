f5cb4b2b1c085c50837ee843fadf36904bc1749d
Korijn van Golen <korijn@gmail.com>
2016-04-18 14:02:43 +0200

    typo

--- psycopg2/bld.bat
+++ psycopg2/bld.bat
@@ -7,7 +7,7 @@ if %ARCH%==32 (
     set ARCH_NAME=win32
 )
 if %ARCH%==64 (
-    set ARCH_NAME=win-adm64
+    set ARCH_NAME=win-amd64
 )
 
 powershell -Command "$progressPreference = 'silentlyContinue'; Invoke-WebRequest http://www.stickpeople.com/projects/python/win-psycopg/2.6.1/psycopg2-2.6.1.%ARCH_NAME%-%VERSION_NAME%.exe -OutFile installer.exe"
