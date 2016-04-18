a6e96f234b6a34d7cff61507d17ecee662576eb9
Korijn van Golen <korijn@gmail.com>
2016-04-18 13:57:49 +0200

    Updated psycopg2 windows package

--- psycopg2/bld.bat
+++ psycopg2/bld.bat
@@ -1,8 +1,17 @@
-"%PYTHON%" setup.py install
+set VERSION_NAME=py%PY_VER%-pg9.4.4-release
+if %PY_VER%==3.5 (
+    set VERSION_NAME=py3.5
+)
+
+if %ARCH%==32 (
+    set ARCH_NAME=win32
+)
+if %ARCH%==64 (
+    set ARCH_NAME=win-adm64
+)
+
+powershell -Command "$progressPreference = 'silentlyContinue'; Invoke-WebRequest http://www.stickpeople.com/projects/python/win-psycopg/2.6.1/psycopg2-2.6.1.%ARCH_NAME%-%VERSION_NAME%.exe -OutFile installer.exe"
+
+easy_install installer.exe
+
 if errorlevel 1 exit 1
-
-:: Add more build steps here, if they are necessary.
-
-:: See
-:: http://docs.continuum.io/conda/build.html
-:: for a list of environment variables that are set during the build process.
