1fa0060b90fab7e74e80481e4be79e45a1d12566
Maxime <maxyme@outlook.com>
2016-04-26 15:57:18 +0200

    changed recipe for invoke.

--- invoke/bld.bat
+++ invoke/bld.bat
@@ -1,9 +1,8 @@
-IF %PY3K% NEQ 1 (
-    RD /S /Q .\invoke\vendor\yaml3
-)
-IF %PY3K% NEQ 0 (
-    RD /S /Q .\invoke\vendor\yaml2
-)
-
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
+
+:: Add more build steps here, if they are necessary.
+
+:: See
+:: http://docs.continuum.io/conda/build.html
+:: for a list of environment variables that are set during the build process.
