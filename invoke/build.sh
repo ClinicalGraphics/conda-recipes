27cfa9c4614c3a34ff55d433f0dfa9e6c8791f77
Maxime <maxyme@outlook.com>
2016-04-26 15:57:07 +0200

    changed recipe for invoke.

--- invoke/build.sh
+++ invoke/build.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-if [ $PY3K -eq 1 ]
-    rm -rf .\invoke\vendor\yaml2
-fi
-if [ $PY3K -eq 0 ]
-    rm -rf .\invoke\vendor\yaml3
-fi
-
 $PYTHON setup.py install
 
 # Add more build steps here, if they are necessary.
@@ -14,3 +7,4 @@ $PYTHON setup.py install
 # See
 # http://docs.continuum.io/conda/build.html
 # for a list of environment variables that are set during the build process.
+
