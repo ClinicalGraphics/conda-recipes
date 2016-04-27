2cc998ec9f30aa82457f4c6c56c3964f72eb4c30
Ivo Flipse <ivoflipse@gmail.com>
2016-04-27 10:45:20 +0200

    Removed a line that wasn't necessary here

--- miktex-portable/bld.bat
+++ miktex-portable/bld.bat
@@ -7,8 +7,5 @@ if errorlevel 1 exit 1
 xcopy /e /i "%SRC_DIR%\*.*" "%PREFIX%\miktex-portable\"
 if errorlevel 1 exit 1
 
-del "%UNITTESTDATA_DIR%\bld.bat"
-if errorlevel 1 exit 1
-
 for %%f in ("%PREFIX%\miktex-portable\miktex\bin\*.exe") do echo %%f %%* >> "%SCRIPTS%\%%~nf.bat"
 if errorlevel 1 exit 1
