66096285dd6bab424a22748e996efecac7893988
Maxime <maxyme@outlook.com>
2016-06-30 13:54:58 +0200

    added win32 package for selenium chromedriver.

--- selenium-chromedriver/bld.bat
+++ selenium-chromedriver/bld.bat
@@ -1,2 +1,10 @@
-python setup.py install
+REM SET install_dir=C:\Program Files (x86)\selenium\chromedriver
+
+REM MKDIR "%install_dir%"
+
+for /R "%SRC_DIR%" %%f in (*.exe) do copy %%f "%PYTHONPATH%/Scripts"
 if errorlevel 1 exit 1
+
+REM ECHO "Adding chromedriver install directory to the persistent path:" %install_dir%
+REM setx /M PATH "%install_dir%;%PATH%"
+REM if errorlevel 1 exit 1
