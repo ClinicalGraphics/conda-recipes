3b9f7f77ffa62a354bd047e6ece1d43b28b7311f
Maxime <maxyme@outlook.com>
2016-07-07 10:24:20 +0200

    Fixed windows version of chromedriver.

--- selenium-chromedriver/bld.bat
+++ selenium-chromedriver/bld.bat
@@ -2,7 +2,7 @@ REM SET install_dir=C:\Program Files (x86)\selenium\chromedriver
 
 REM MKDIR "%install_dir%"
 
-for /R "%SRC_DIR%" %%f in (*.exe) do copy %%f "%PYTHONPATH%/Scripts"
+for /R "%SRC_DIR%" %%f in (*.exe) do copy %%f "%LIBRARY_BIN%"
 if errorlevel 1 exit 1
 
 REM ECHO "Adding chromedriver install directory to the persistent path:" %install_dir%
