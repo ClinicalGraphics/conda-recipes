80885a163ca87b00e392c3daa58164146003b3e5
Maxime <maxyme@outlook.com>
2016-09-20 13:48:55 +0200

    test vtku3dwithout setfilename.

--- vtku3dexporter/run_test.py
+++ vtku3dexporter/run_test.py
@@ -3,40 +3,39 @@ import os
 import vtku3dexporter
 import tempfile
 
-# create cube
-cube = vtk.vtkCubeSource()
 
-# mapper
-cubeMapper = vtk.vtkPolyDataMapper()
-cubeMapper.SetInputData(cube.GetOutput())
-
-# actor
-cubeActor = vtk.vtkActor()
-cubeActor.SetMapper(cubeMapper)
-
-
-def write_u3d(destination_path, actor):
+def write_u3d(destination_folder, filename, actor):
     render_window = vtk.vtkRenderWindow()
     render_window.OffScreenRenderingOn()
     renderer = vtk.vtkRenderer()
     render_window.AddRenderer(renderer)
     renderer.AddActor(actor)
-
     renderer.ResetCamera()
-    render_window.Render()
     u3d_exporter = vtku3dexporter.vtkU3DExporter()
-    u3d_file_path = os.path.join(destination_path, "report")
-    u3d_exporter.SetFileName(u3d_file_path)
+    file_path = os.path.join(destination_folder, filename)
+    #u3d_exporter.SetFileName(file_path)
     u3d_exporter.SetInput(render_window)
-    print(u3d_file_path)
-    print("writing...")
+    print("writing file to {}.u3d".format(file_path))
     u3d_exporter.Write()
-    print("test")
+    print("testing that file exists...")
     # Check that we have successfully created a U3D file
-    if not os.path.exists("{}.u3d".format(u3d_file_path)):  # The extension get's added by the Exporter itself
+    if not os.path.exists("{}.u3d".format(file_path)):  # The extension get's added by the Exporter itself
         raise Exception("Failed to create the U3D file")
+    print("test completed")
 
 
-dir_path = tempfile.gettempdir() # prints the current temporary directory
+# Create cube
+cube = vtk.vtkCubeSource()
 
-write_u3d(dir_path, cubeActor)
+# Mapper
+cubeMapper = vtk.vtkPolyDataMapper()
+cubeMapper.SetInputData(cube.GetOutput())
+
+# Actor
+cubeActor = vtk.vtkActor()
+cubeActor.SetMapper(cubeMapper)
+
+# Write u3d
+dir_path = tempfile.gettempdir()
+os.chdir(dir_path)
+write_u3d(dir_path, "test_report", cubeActor)
