9cf4fc440ed02c39859c37da1c8620be4391e095
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2016-12-22 16:08:01 +0100

    Added missing dependencies and fixed Windows script. Linux is broken?

--- vtku3dexporter/bld.bat
+++ vtku3dexporter/bld.bat
@@ -3,6 +3,22 @@ set BUILD_CONFIG=Release
 REM tell cmake where Python is
 set PYTHON_LIBRARY=%CENV%\libs\python%PY_VER:~0,1%%PY_VER:~2,1%.lib
 
+REM Build u3d First
+mkdir build
+cd build
+
+cmake .. -G "NMake Makefiles" ^
+    -Wno-dev ^
+    -DCMAKE_BUILD_TYPE=%BUILD_CONFIG% ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DU3D_SHARED:BOOL=ON
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
+
+REM Then build vkt to u3d exporter
+cd ..
 cd Samples\SampleCode
 
 cmake . -G "NMake Makefiles" ^
