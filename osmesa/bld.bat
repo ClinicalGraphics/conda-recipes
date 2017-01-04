db28cb6b55ca025e4e694971d700344f69541f9e
maxyme <maxyme@hotmail.com>
2017-01-04 17:03:48 +0100

    updated osmesa recipe - works for windows.

--- osmesa/bld.bat
+++ osmesa/bld.bat
@@ -1,11 +1,14 @@
-#mingw-get install win_flex win-bison
-#msys-flex msys-bison
+:: LLVM linking not working somehow
+:: SET LLVM="%LIBRARY_PREFIX%\lib\"
 
-#scons llvm=yes build=release openmp=true osmesa
-scons platform=windows toolchain=crossmingw machine=x86 libgl-gdi
+:: Have to use CALL to prevent the script from exiting after calling SCons
+CALL scons build platform=windows build=release openmp=true osmesa libgl-gdi
 
-# move the driver
-move "%PREFIX%\lib\python\libOSMesa.dll" "%PREFIX%\lib\"
+:: move the osmesa.dll and other files generated to LIBRARY_BIN
+xcopy "build\windows-x86_64\gallium\targets\osmesa" "%LIBRARY_BIN%"\ /O /X /E /H /K
 
-# move the include files
-move "%PREFIX%\lib\python\libosmesa.dll" "%PREFIX%\include\GL\"
+:: move the opengl32.dll and other files generated to LIBRARY_BIN
+xcopy "build\windows-x86_64\gallium\targets\libgl-gdi" "%LIBRARY_BIN%"\ /O /X /E /H /K
+
+:: move the header files for mesa to LIBRARY_INC\GL
+xcopy include\GL "%LIBRARY_INC%"\GL\ /O /X /E /H /K
