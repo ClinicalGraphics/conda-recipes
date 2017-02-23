4d264f1af4d272dadb34f3dd975d9d49ab7b3ca9
Berend Klein Haneveld <berendkleinhaneveld@clinicalgraphics.com>
2017-02-23 14:05:45 +0100

    Updated the recipe for u3d so that it also compiles properly on macOS

--- u3d/build.sh
+++ u3d/build.sh
@@ -4,9 +4,16 @@ BUILD_CONFIG=Release
 mkdir build
 cd build
 
-cmake .. -G "Unix Makefiles" \
-    -DCMAKE_BUILD_TYPE=$BUILD_CONFIG \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+if [ `uname` = 'Darwin' ]; then
+    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+fi
+
+cmake .. -G "Ninja" \
+    -DCMAKE_BUILD_TYPE=${BUILD_CONFIG} \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_INSTALL_RPATH:PATH="${PREFIX}/lib" \
+    -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+    ${MACOSX_DEPLOYMENT_TARGET:+-DCMAKE_OSX_DEPLOYMENT_TARGET='10.9'} \
     -DU3D_SHARED:BOOL=ON
 
-make install
+ninja install
