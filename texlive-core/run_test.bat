2dfff8966ae7078c4045a847c272fde44e9e70df
Berend Klein Haneveld <berendkleinhaneveld@clinicalgraphics.com>
2017-12-07 15:04:19 +0100

    Exit when something goes wrong in the test

--- texlive-core/run_test.bat
+++ texlive-core/run_test.bat
@@ -1,7 +1,12 @@
 :: Apparently, only the first test command from meta.yml is run
 :: So here are all the test commands
 pdftex --version
+if errorlevel 1 exit 1
 bibtex --version
+if errorlevel 1 exit 1
 tlmgr --version
+if errorlevel 1 exit 1
 pdflatex --version
+if errorlevel 1 exit 1
 pdflatex template.tex
+if errorlevel 1 exit 1
