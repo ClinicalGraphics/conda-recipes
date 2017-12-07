e18ec41fe9573efec7ae1e3d96a0032d66694407
Berend Klein Haneveld <berendkleinhaneveld@clinicalgraphics.com>
2017-12-07 14:27:16 +0100

    Fixes for Windows build, update version and remove post-link script

--- texlive-selected/bld.bat
+++ texlive-selected/bld.bat
@@ -2,17 +2,23 @@
 mkdir %LIBRARY_PREFIX%/tlpkg/backups
 tlmgr update --self
 
+if errorlevel 1 exit 1
+
 :: Download and unpack Acrotex package
-mkdir %LIBRARY_PREFIX%/texmf-local/tex/latex/local
-cd %LIBRARY_PREFIX%/texmf-local/tex/latex/local
-python -c "import urllib; urllib.urlretrieve('http://mirrors.ctan.org/macros/latex/contrib/acrotex.zip', 'acrotex.zip')"
+mkdir %LIBRARY_PREFIX%\texmf-local\tex\latex\local
+cd %LIBRARY_PREFIX%\texmf-local\tex\latex\local
+python -c "import urllib.request; urllib.request.urlretrieve('http://mirrors.ctan.org/macros/latex/contrib/acrotex.zip', 'acrotex.zip')"
 python -c "import zipfile; zipfile.ZipFile('acrotex.zip').extractall()"
-rm acrotex.zip
+del acrotex.zip
 
 :: Install the Acrotex package
 cd acrotex
 texhash
 latex acrotex.ins
 
+if errorlevel 1 exit 1
+
 :: Install some needed texlive packages with tlmgr
 tlmgr install symbol conv-xkv media9 ocgx2 enumitem kurier adjustbox collectbox wallpaper lastpage multirow fontawesome threeparttable animate titling
+
+if errorlevel 1 exit 1
---
+++ texlive-selected/run_test.bat
@@ -0,0 +1,5 @@
+:: Apparently, only the first test command from meta.yml is run
+:: So here are all the test commands
+pdflatex --version
+pdftex --version
+pdflatex template.tex
